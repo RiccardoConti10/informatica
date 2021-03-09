7cd673316692e1c3c1859f92d7f50db54fbe64c2
Riccardo Conti <74264750+RiccardoConti10@users.noreply.github.com>
2021-03-09 19:44:18 +0100

    Create es_3_pag_292.py

---
+++ es_3_pag_292.py
@@ -0,0 +1,17 @@
+''' Aggiungi alla classe Atleta un metodo chiamato effettua_visita che ponga l'attributo visitaMedica uguale a True'''
+class Atleta () :
+    def __init__(self, altezza, sport, peso, squadra, visitaMedica):
+        self.altezza = altezza
+        self.sport = sport
+        self.peso = peso
+        self.squadra = squadra
+        self.visitaMedica = visitaMedica
+    def effettua_visita (self) :
+        while self.visitaMedica == True :
+            risp = int(input ("L'Atleta ha effettuato la visita medica? Cliccare 1 per dire di sì, cliccare qualsiasi altro tasto per dire di no"))
+            if self.visitaMedica == 1 :
+                print ("L'Atleta  ha effettuato la visita medica, può giocare")
+            else :
+                print ("L'Atleta non ha effetuato la visita medica, quindi non può giocare")
+                break
+    effettua_visita() 

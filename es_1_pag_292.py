44c221b1ef3cb67ff53e9fb2ac367acbef590625
Riccardo Conti <74264750+RiccardoConti10@users.noreply.github.com>
2021-03-09 19:40:02 +0100

    Create es_1_pag_292.py

---
+++ es_1_pag_292.py
@@ -0,0 +1,7 @@
+ class Atleta () :
+    def __init__(self, altezza, sport, peso, squadra, visitaMedica):
+        self.altezza = altezza
+        self.sport = sport
+        self.peso = peso
+        self.squadra = squadra
+        self.visitaMedica = visitaMedica
